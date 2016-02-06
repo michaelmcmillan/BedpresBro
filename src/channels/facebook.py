6d80a6f6f0aac5e6dec08780cab04c5d346b8837
Michael McMillan <email@michaelmcmillan.net>
2016-02-06 17:37:09 +0100

    Make a prettier notification message

--- src/channels/facebook.py
+++ src/channels/facebook.py
@@ -10,5 +10,7 @@ class Facebook:
         self.thread = configuration['facebook']['message_thread']
 
     def notify(self, event):
-        self.client.send(self.thread, event.title)
+        self.client.send(self.thread,
+            'Påmelding til "%s" begynner om under 20 minutter' % event.title
+        )
         event.notification_sent = True
