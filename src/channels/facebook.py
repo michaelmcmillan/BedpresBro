493881d6258bd21b4b8a93499f614d11483d3805
Michael McMillan <email@michaelmcmillan.net>
2016-02-06 18:57:14 +0100

    add full stop to message

--- src/channels/facebook.py
+++ src/channels/facebook.py
@@ -11,6 +11,6 @@ class Facebook:
 
     def notify(self, event):
         self.client.send(self.thread,
-            'Påmelding til "%s" begynner om under 20 minutter' % event.title
+            'Påmelding til "%s" begynner om under 20 minutter.' % event.title
         )
         event.notification_sent = True
